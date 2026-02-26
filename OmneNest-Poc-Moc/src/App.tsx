0bc6df1a2607c6da66849bcbd93cade3872a9335
Dhruv-Mehta13 <dhruv.mehta@omnenest.com>
2026-02-26 23:06:49 +0530

    Ticket -049

--- OmneNest-Poc-Moc/src/App.tsx
+++ OmneNest-Poc-Moc/src/App.tsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import AdvancedComponent from "./components/AdvancedComponent"
 import AreaChart from "./components/AreaChart"
 import CandleChart from "./components/Candle"
+import ComparisonChart from "./components/ComparisonChart";
 import LineChart from "./components/LineChart"
-
-
 function App() {
+  const [symbols] = useState(["BTCUSDT", "ETHUSDT", "BNBUSDT"]);
   return (
     <div>
       <AreaChart/>
       <CandleChart/>
       <LineChart/>
       <AdvancedComponent/>
+         <div style={{ padding: "20px", background: "#ffffff", minHeight: "100vh" }}>
+      <h2 style={{ color: "white" }}>Overlay Comparison Chart</h2>
+      <ComparisonChart symbols={symbols} />
+    </div>
     </div>
   )
 }
